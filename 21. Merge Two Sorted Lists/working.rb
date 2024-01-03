48faeca16c4fe3afa5189f51b27261af20179be4
Bindles <bindlez@gmail.com>
2024-01-03 00:38:03 -0500

    working-21. Merge Two Sorted Lists-close

---
+++ 21. Merge Two Sorted Lists/working.rb
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} list1
+# @param {ListNode} list2
+# @return {ListNode}
+def merge_two_lists(list1 = [1,2,4], list2 = [1,3,4])
+  p list1
+  p list2
+  p list1.class
+    
+end
+
+p merge_two_lists
