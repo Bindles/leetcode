42d3f5a73ae1d68038052817ec3979a0b9bdefe3
Bindles <bindlez@gmail.com>
2024-01-03 08:52:56 -0500

    working-2942. Find Words Containing Character

---
+++ 26. Remove Duplicates from Sorted Array/2942. Find Words Containing Character/working.rb
@@ -0,0 +1,18 @@
+# @param {String[]} words
+# @param {Character} x
+# @return {Integer[]}
+def find_words_containing(words = ["leet","code", "sa"], x = "e")
+  result=[]
+  words.each_index do |i|
+    p words[i] && result << words[i] if words[i].include?(x)
+  end
+  result
+end
+
+def find_words_containing(words = ["leet","code", "sa"], x = "e")
+  result=[]
+  words.each.with_index { |v, i| p v,i && result << i if words[i].include?(x) }
+  return result
+end
+
+p find_words_containing
