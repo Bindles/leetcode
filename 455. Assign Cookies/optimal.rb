2b6314252a27647daabeba840703d0ed539614f8
Bindles <bindlez@gmail.com>
2024-01-06 16:35:46 -0500

    optinal-455

---
+++ 455. Assign Cookies/optimal.rb
@@ -0,0 +1,17 @@
+def find_content_children(g, s)
+  g.sort!
+  s.sort.inject(0) {|count,size|
+    break count if !g[count]
+    count +=1 if size >= g[count]
+    count
+}
+end
+
+def find_content_children(g, s)
+  g.sort!
+  s.sort.reject{|size| size < g[0]}.inject(0) {|count,size|
+    break count if !g[count]
+    count +=1 if size >= g[count]
+    count
+  }
+end
