a5020b368f188bb0a9fd7b2cf566138d4c1f1e3e
Bindles <bindlez@gmail.com>
2024-01-09 20:36:22 -0500

    SOLVED-x2-sets-2506-Count-Pairs-of-Similar-Strings

--- 2506. Count Pairs Of Similar Strings/solved.rb
+++ 2506. Count Pairs Of Similar Strings/solved.rb
@@ -12,7 +12,7 @@ def similar_pairs(words = ["aba","aabb","abcd","bac","aabc"])
     p wordmap
     wordmaps << wordmap
   end
-  p wordmaps[1]
+  p wordmaps
 
   wordmaps.each.with_index do |wm, ind|
     (ind + 1).upto(wordmaps.length - 1) do |compare_ind|
@@ -27,3 +27,52 @@ def similar_pairs(words = ["aba","aabb","abcd","bac","aabc"])
 end
 similar_pairs
 
+#MY SOLUTION #2 USE SETS {+}7min
+def similar_pairs(words = ["aba","aabb","abcd","bac","aabc"])
+  wordmaps=[]
+  result=0
+
+  words.each do |word|
+    p "#{word} =>"
+    wordmap = Hash.new(0)
+    word.each_char {|char| wordmap[char]+=1}
+    p wordmap
+    wordmaps << wordmap
+  end
+  p wordmaps
+
+  wordmaps.each.with_index do |wm, ind|
+    (ind + 1).upto(wordmaps.length - 1) do |compare_ind|
+      if Set.new(wordmaps[ind].keys) == Set.new(wordmaps[compare_ind].keys)
+        puts "Word maps #{ind + 1} and #{compare_ind + 1} have the same set of letters:"
+        result+=1
+        p wordmaps[ind]
+      end
+    end
+  end
+  p result
+end
+similar_pairs
+
+#MY SOLUTION #1|DEBUG PUTS REMOVED
+def similar_pairs(words = ["aba","aabb","abcd","bac","aabc"])
+  wordmaps=[]
+  result=0
+
+  words.each do |word|
+    wordmap = Hash.new(0)
+    word.each_char {|char| wordmap[char]+=1}
+    wordmaps << wordmap
+  end
+
+  wordmaps.each.with_index do |wm, ind|
+    (ind + 1).upto(wordmaps.length - 1) do |compare_ind|
+      if wordmaps[ind].keys.sort == wordmaps[compare_ind].keys.sort
+        puts "Word maps #{ind + 1} and #{compare_ind + 1} have the same set of letters:"
+        result+=1
+        p wordmaps[ind]
+      end
+    end
+  end
+  p result
+end
