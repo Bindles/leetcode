2e5407adccb7bd7493399e6ccbbb60c72009b348
Bindles <bindlez@gmail.com>
2024-01-17 10:19:31 -0500

    SOLVED-AGAIN-HASH-ONELINER-1207. Unique Number of Occurrences

--- 1207. Unique Number of Occurrences.rb
+++ 1207. Unique Number of Occurrences.rb
@@ -11,7 +11,7 @@ def unique_occurrences(arr)
   p result.uniq
   p result.size ==  result.uniq.size
 end
-p unique_occurrences([1,2,2,1,1,3])
+unique_occurrences([1,2,2,1,1,3])
 #CONCISE
 def unique_occur(arr)
   arruniq, result = arr.uniq,[]
@@ -19,6 +19,21 @@ def unique_occur(arr)
 end
 p unique_occur([1,2,2,1,1,3])
 
+#USEHASH BELOW
+def unique_occurrences_playground(arr)
+  p arr.tally
+  p arr.tally.values
+  p arr.tally.values.uniq
+end
 
+def unique_occurrences(arr)
+  arrtally = arr.tally
+  arrtally.values.size ==  arrtally.values.uniq.size
+end
 
+def unique_occurrencesx(arr)
+  arr.uniq.size == arr.tally.values.uniq.size
+end 
+
+p unique_occurrencesx([1,2,2,1,1,3])
 
