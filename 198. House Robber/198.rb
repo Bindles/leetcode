4caf2461ac4d85abb65150a1a93545376d0f4fa4
Bindles <bindlez@gmail.com>
2024-01-21 20:36:27 -0500

    CONCISE-198-House-Robber

--- 198. House Robber/198.rb
+++ 198. House Robber/198.rb
@@ -1,6 +1,6 @@
 # @param {Integer[]} nums
 # @return {Integer}
-#MY-SOLUTION
+#MY-SOLUTION => PUTS
 def rob(nums = [1, 2, 3, 1])
   puts "Input array: #{nums}"
 
@@ -32,4 +32,25 @@ def rob(nums = [1, 2, 3, 1])
   result
 end
 rob
+
+
+#MY-SOLUTION-CONCISE
+def rob(nums = [1, 2, 3, 1])
+  return 0 if nums.empty?
+  n = nums.length
+  return nums[0] if n == 1
+
+  dp = Array.new(n, 0)
+  dp[0] = nums[0]
+  dp[1] = [nums[0], nums[1]].max
+
+  (2...n).each do |i|
+    dp[i] = [dp[i-1], dp[i-2] + nums[i]].max
+  end
+
+  result = [dp[-1], dp[-2]].max
+  return result
+end
+rob
+
 
