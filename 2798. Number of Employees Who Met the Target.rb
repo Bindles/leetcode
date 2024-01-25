87fddb245561a7401e3f6425449c6f2e98ac0eb6
Bindles <bindlez@gmail.com>
2024-01-25 00:22:35 -0500

    2798. Number of Employees Who Met the Target  - [22 SEC] => (concise)

--- 2798. Number of Employees Who Met the Target.rb
+++ 2798. Number of Employees Who Met the Target.rb
@@ -1,22 +1,35 @@
 # @param {Integer[]} hours
 # @param {Integer} target
 # @return {Integer}
+
 #MY_SOLUTION => 22 seconds
-a=[]
-hours.each do |h|
-  a << h if h >=target
-end
-a.size
-
-
-
-def number_of_employees_who_met_target(hours, target)
+def number_of_employees_who_met_target(hours = [0,1,2,3,4], target = 2)
   a=[]
-      hours.each do |h|
-      p h
-      a << h if h >= target
-      end
-      a.size
+  hours.each do |h|
+    p h
+    a << h if h >= target
+  end
+  a.size
 end
-number_of_employees_who_met_target
+p number_of_employees_who_met_target
+
+#CONCISE
+def number_of_employees_who_met_target(hours = [0,1,2,3,4], target = 2)
+  a=[];hours.each {|h|a << h if h >= target} && a.size
+end
+p number_of_employees_who_met_target
+
+
+#SELECT_BETTER
+def number_of_employees_who_met_target(hours = [0,1,2,3,4], target = 2)
+  hours.select do |h|
+    h >= target
+  end.size
+end
+p number_of_employees_who_met_target
+
+#SELECT=>CONCISE
+def number_of_employees_who_met_target(hours = [0,1,2,3,4], target = 2)
+  hours.select {|hour|hour>=target}.size
+end
 
