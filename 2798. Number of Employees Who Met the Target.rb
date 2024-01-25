47e209880fe47bc245e0ed3c3fac19a35b5d5365
Bindles <bindlez@gmail.com>
2024-01-25 00:04:53 -0500

    2798-solved in 22 seconds in leetcode editor-correct end format error when copying to vs code

--- 2798. Number of Employees Who Met the Target.rb
+++ 2798. Number of Employees Who Met the Target.rb
@@ -1,6 +1,15 @@
 # @param {Integer[]} hours
 # @param {Integer} target
 # @return {Integer}
+#MY_SOLUTION => 22 seconds
+a=[]
+hours.each do |h|
+  a << h if h >=target
+end
+a.size
+
+
+
 def number_of_employees_who_met_target(hours, target)
   a=[]
       hours.each do |h|
@@ -8,4 +17,6 @@ def number_of_employees_who_met_target(hours, target)
       a << h if h >= target
       end
       a.size
-  end
+end
+number_of_employees_who_met_target
+
