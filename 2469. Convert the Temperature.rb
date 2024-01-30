9807ae88a6e36db841b88c0b458e6f9184895484
Bindles <bindlez@gmail.com>
2024-01-30 06:26:23 -0500

    1689. Partitioning Into Minimum Number Of Deci-Binary Numbers-FAST-40sec

--- 2469. Convert the Temperature.rb
+++ 2469. Convert the Temperature.rb
@@ -2,3 +2,10 @@
 def convert_temperature(celsius)
   res=[]; res << celsius + 273.15; res << celsius * 1.80 + 32.00 and res
 end
+
+
+p 123456.digits.select  {|i| i % 3 == 0} 
+p 123.digits.find  {|i| i % 3 == 0} 
+p [1,2,3].map  {|i| i % 3 == 0}
+p [1,2,3].map(&:even?)
+p [1,2,3].map(&:class)    # MY SOLUTION
