f987fcef88c614e7a7c3a00a27eabd8244d326ca
Bindles <bindlez@gmail.com>
2024-01-30 06:13:24 -0500

    2469. Convert the Temperature-FAST-23SEC-DONE

--- 1920. Build Array from Permutation.rb
+++ 1920. Build Array from Permutation.rb
@@ -3,3 +3,9 @@
 def build_array(nums)
   ans=[]; nums.each.with_index{|num,i| p ans[i] = nums[nums[i]] } and ans
 end
+
+# @param {Float} celsius
+# @return {Float[]}
+def convert_temperature(celsius)
+  res=[]; res << celsius + 273.15; res << celsius * 1.80 + 32.00 and res
+end
---
+++ 2469. Convert the Temperature.rb
@@ -0,0 +1,4 @@
+#MY_SOLUTION 
+def convert_temperature(celsius)
+  res=[]; res << celsius + 273.15; res << celsius * 1.80 + 32.00 and res
+end
