b95be3c0a6845e8bc2972ff277015cf1fd04c0fc
Bindles <bindlez@gmail.com>
2024-02-01 00:00:04 -0500

    2966. Divide Array Into Arrays With Max Difference-WORKING-STILL

--- 2966.rb
+++ 2966.rb
@@ -7,7 +7,30 @@ def divide_array(nums = [1,3,4,8,7,9,3,5,1], k = 2)
     # p nums.each_cons(3) {|a| res << a if a.size==3}
      p res
 end
+
 p divide_array
+incz=0
+splittimes= (2)
+i=0
+a=[1,2,3,4,5,6]
+b=[]
+while i < splittimes do
+  b << a.slice(incz,incz+3)
+  incz+=3
+  i+=1
+end
+
+#b << a.slice(incz,incz+3)
+p b
+
+(0..splittimes-1).each do |splitem|
+  p b[splitem]
+  p b[splitem].min
+  p b[splitem].max
+  p b[splitem].max - b[splitem].min <=3
+  p b if b[splitem].max - b[splitem].min <=3
+  p '--'
+end
 
 def zzz(nums = [1,3,4,8,7,9,3,5,1], k = 2)
 
@@ -64,9 +87,3 @@ def zzz(nums = [1,3,4,8,7,9,3,5,1], k = 2)
   end
   
   zzzz()
-  
-
-
-
-end
-p zzz
