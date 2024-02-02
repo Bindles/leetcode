1657bb7bb6d747bbd07c8985ff4fb7d499fe268a
Bindles <bindlez@gmail.com>
2024-02-02 15:04:23 -0500

    2520. Count the Digits That Divide a Number

---
+++ 2520. Count the Digits That Divide a Number.rb
@@ -0,0 +1,31 @@
+#2520. Count the Digits That Divide a Number
+# @param {Integer} num
+# @return {Integer}
+
+#FIRST SOLUTION, DECIDED TO USE SELECT WHICH IS FAIRLY NEW TO ME, AMAZING, 30SEC TOTAL TIME
+def count_digits(num= 121)
+  num.digits.reverse.select{|a| num % a == 0}.count
+end
+
+
+#TRYIING W.O SELECT FOR COMPARISON
+def count_digits(num= 121)
+  arr=[];  num.digits.reverse.each {|a| arr << a if num % a == 0} and arr.count
+end
+p count_digits
+
+
+
+#MORE SELECT
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+prime_numbers = numbers.select do |num|
+  next if num <= 1
+  (2..Math.sqrt(num)).none? { |i| num % i == 0 }
+end
+
+p prime_numbers
+
+# Doubling even numbers
+numbers = [1, 2, 3, 4, 5]
+doubled_even_numbers = numbers.select { |num| num.even? }.map { |num| num * 2 }
+p doubled_even_numbers # Outputs [4,  2
--- _topics/select.rb
+++ _topics/select.rb
@@ -1,3 +1,56 @@
+# Understanding when to use select versus each depends on the task you're trying to accomplish. Here are some patterns and scenarios where you might find select to be more appropriate than each:
+
+#REAL_SOLUTION_LC_COUNT_DIGITS
+# Given an integer num, return the number of digits in num that divide num.
+# An integer val divides nums if nums % val == 0.
+
+# Input: num = 121
+# Output: 2
+# Explanation: 121 is divisible by 1, but not 2. Since 1 occurs twice as a digit, we return 2.
+def count_digits(num= 121)
+  num.digits.reverse.select{|a| num % a == 0}.count
+end
+
+# Filtering elements: If you need to filter elements from a collection based on a condition, select is often a better choice. It returns a new array containing only the elements that satisfy the condition.
+
+# Using select to filter even numbers
+numbers = [1, 2, 3, 4, 5]
+even_numbers = numbers.select { |num| num.even? }
+p even_numbers # Output: [2, 4]
+# Counting elements: If you need to count the number of elements that satisfy a condition, select followed by count is a concise way to achieve this.
+
+# Counting even numbers
+numbers = [1, 2, 3, 4, 5]
+count_even_numbers = numbers.select { |num| num.even? }.count
+p count_even_numbers # Output: 2
+# Finding elements: If you need to find elements that match a certain criterion, select can be used to easily obtain those elements.
+
+# Finding prime numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+prime_numbers = numbers.select do |num|
+  next if num <= 1
+  (2..Math.sqrt(num)).none? { |i| num % i == 0 }
+end
+p prime_numbers
+# Transforming elements: If you need to transform elements based on a condition, select followed by map or collect is a common pattern.
+
+
+# Doubling even numbers
+numbers = [1, 2, 3, 4, 5]
+doubled_even_numbers = numbers.select { |num| num.even? }.map { |num| num * 2 }
+p doubled_even_numbers
+# Checking for existence: If you need to check whether any elements in a collection satisfy a condition, select followed by any? can be useful.
+
+
+# Checking if there are any negative numbers
+numbers = [1, 2, -3, 4, 5]
+has_negative_numbers = numbers.select { |num| num < 0 }.any?
+p has_negative_numbers
+# Overall, select is particularly useful when you need to work with subsets of data that meet specific criteria, such as filtering, counting, or transforming elements in a collection. It's a powerful method for working with enumerable objects in a concise and expressive way.
+
+
+
+
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # Simple Example
