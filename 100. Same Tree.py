3aa9f4537a50c62d881e11665fc77ce60ab29762
Bindles <bindlez@gmail.com>
2024-02-26 09:36:18 -0500

    100. Same Tree-PYTHON

---
+++ 100. Same Tree.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#*MY SOL
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None and q is None:
+            return True
+        if p is None or q is None or p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+#* TO RUN =>
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# Define the isSameTree function
+def isSameTree(p=[1, 2, 3], q=[1, 2, 3]) -> bool:
+    if p is None and q is None:
+        return True
+    if p is None or q is None or p.val != q.val:
+        return False
+    return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+
+# Create TreeNode instances for p and q
+p = TreeNode(1, TreeNode(2), TreeNode(3))
+q = TreeNode(1, TreeNode(2), TreeNode(3))
+
+# Call the isSameTree function
+result = isSameTree(p, q)
+print(result)
