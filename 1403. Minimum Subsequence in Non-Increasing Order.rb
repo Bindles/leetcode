fc4edad53c563d4dfa65b69a70d1c452a06de9b7
Bindles <bindlez@gmail.com>
2024-03-16 10:03:19 -0400

    1403. Minimum Subsequence in Non-Increasing Order-RUBY

---
+++ 1403. Minimum Subsequence in Non-Increasing Order.rb
@@ -0,0 +1,75 @@
+#* 1403. Minimum Subsequence in Non-Increasing Order
+nums = [4,4,7,6,7]
+# @param {Integer[]} nums
+# @return {Integer[]}
+#* SOL 1
+def min_subsequence(nums)
+  nums.sort!.reverse! 
+  sum = nums.sum
+  subsequence_sum = 0
+  result = []
+  
+  nums.each do |num|
+    subsequence_sum += num
+    result << num
+
+    return result if subsequence_sum > sum / 2
+  end
+end
+
+#* Sol 1.1 | Puts | Comments
+def min_subsequence(nums)
+  nums.sort!.reverse! # Sort the array in non-increasing order
+  sum = nums.sum
+  subsequence_sum = 0
+  result = []
+  
+  nums.each do |num|
+    p subsequence_sum += num
+    p result << num
+    puts "Subsequence: #{result}, Subsequence Sum: #{subsequence_sum}"
+
+    # If the sum of the subsequence exceeds half of the total sum, return the result
+    if subsequence_sum > sum / 2
+      puts "Final Result: #{result}"
+      return result 
+    end
+  end
+end
+puts min_subsequence(nums)  # Output: [10, 9]
+
+
+#* WORKING ..
+#* 1
+def min_subsequence(nums)
+  sum = nums.sum
+  (0...nums.length-1).each do |i|
+    p nums[i..i+1], nums[i..i+1].sum
+    p "#{(sum - nums[i..i+1].sum) < nums[i..i+1].sum}"
+    return nums[i..i+1] if (sum - nums[i..i+1].sum) < nums[i..i+1].sum
+  end
+end
+p min_subsequence(nums)
+
+#* IN LOOP
+def min_subsequence(nums)
+  inc=1
+  sum = nums.sum
+  while inc < nums.length-1
+    (0...nums.length-inc).each do |i|
+      p nums[i..i+inc], nums[i..i+inc].sum
+      p "#{(sum - nums[i..i+inc].sum) < nums[i..i+inc].sum}"
+      return nums[i..i+inc] if (sum - nums[i..i+inc].sum) < nums[i..i+inc].sum
+    end
+    inc+=1
+  end
+end
+p min_subsequence(nums)
+
+
+#* TESTS
+p nums.sum
+p 34 - 19
+p 34 - 19 < 19
+
+
--- 977. Squares of a Sorted Array.py
+++ 977. Squares of a Sorted Array.py
@@ -4,7 +4,7 @@ nums = [-4,-1,0,3,10]
 # @param {Integer[]} nums
 # @return {Integer[]}
 def sorted_squares(nums):
-  return sorted([num**2 for num in nums], reverse=True)
+  return sorted(num**2 for num in nums)
 
 print(sorted_squares(nums))
 
