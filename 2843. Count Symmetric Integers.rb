30c631ed14a2b86caf1ad6c682cd004f71560655
Bindles <bindlez@gmail.com>
2024-03-15 22:08:50 -0400

    2843. Count Symmetric Integers-RUBY

--- 2843. Count Symmetric Integers.rb
+++ 2843. Count Symmetric Integers.rb
@@ -4,6 +4,23 @@
 # @return {Integer}
 low = 1
 high = 100
+#* SOL 1
+def count_symmetric_integers(low, high)
+  count = 0
+  (low..high).each do |num|
+    next if num.to_s.size.odd?
+    digits = num.to_s.chars.map(&:to_i)
+    half = digits.size / 2
+    sum_left = digits[0...half].sum
+    sum_right = digits[half..].sum
+    count += 1 if sum_left == sum_right
+  end
+  count
+end
+p count_symmetric_integers(low,high)
+
+#*WORKING...
+#* THESE DONT WORK | SOL
 def count_symmetric_integers(low, high)
   res=[]
   (low..high).each do |i|
@@ -14,3 +31,13 @@ def count_symmetric_integers(low, high)
   res
 end
 p count_symmetric_integers(low,high)
+#* SOL 
+def count_symmetric_integers(low, high)
+  count = 0
+  (low..high).each do |i|
+    count += 1 if i.to_s.length > 1 && i.to_s.reverse == i.to_s
+  end
+  count
+end
+p count_symmetric_integers(low,high)
+
