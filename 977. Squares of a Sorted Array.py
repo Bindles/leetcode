75cb2ac9fa1c7f7ab9bee5a6c0797b12776eceb2
Bindles <bindlez@gmail.com>
2024-03-16 09:27:41 -0400

    977. Squares of a Sorted Array-PY

---
+++ 977. Squares of a Sorted Array.py
@@ -0,0 +1,12 @@
+#PYTHON
+#* 977. Squares of a Sorted Array
+nums = [-4,-1,0,3,10]
+# @param {Integer[]} nums
+# @return {Integer[]}
+def sorted_squares(nums):
+  return sorted(num**2 for num in nums)
+
+print(sorted_squares(nums))
+
+
+#sometime bracket is used inside => return sorted([num**2 for num in nums])
