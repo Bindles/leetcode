c93ec3f9a14753f1acfd214af1c80bf72b9b3501
Bindles <bindlez@gmail.com>
2024-03-29 11:57:15 -0400

    120. Triangle-RUBY

--- 120. Triangle.rb
+++ 120. Triangle.rb
@@ -18,3 +18,28 @@ def minimum_total(triangle)
   end[0]
 end
 p minimum_total(triangle)
+
+
+#* SOL | PUTS
+def minimum_total(triangle)
+  triangle.reverse.inject do |bottom_row, upper_row, frow|
+    puts "Bottom row: #{bottom_row}"
+    puts "Upper row: #{upper_row}"
+    zipped_array = upper_row.zip(bottom_row, bottom_row[1..])
+    puts "Zipped array: #{zipped_array}"
+    zipped_array.map do |upper, left, right|
+      upper + [left, right].min
+    end
+  end[0]
+end
+p minimum_total(triangle)
+
+#* TEST 
+def minimum_total(triangle)
+  jj = triangle.reverse.inject do |bottom_row, upper_row|
+    upper_row.zip(bottom_row, bottom_row[1..])
+  end
+  p jj.reduce([], :push)
+  p jj
+end
+p minimum_total(triangle)
---
+++ _topics/python/string.py
@@ -0,0 +1,16 @@
+name = "Cool Guy"
+nam=[1,2,3,4]
+print(len(name))
+print(name.split())
+spname=name.split()
+print(name.capitalize())
+print("".join(spname))
+nam.append("Dude")
+print(nam)
+arr=[1,2,3,4]
+#nam[ {:expression} for {:item} in {:list} if {:conditional} ]
+
+# Example of list comprehension
+result = [x * 100 for x in arr if x % 2 == 0]
+
+print(result)  # Output: [4, 6]
