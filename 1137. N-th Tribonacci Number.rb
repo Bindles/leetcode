2a8cf56536faa1bea2b91cb583776b73c18ae009
Bindles <bindlez@gmail.com>
2024-04-24 17:23:34 -0400

    1137. N-th Tribonacci Number-RUBY

---
+++ 1137. N-th Tribonacci Number.rb
@@ -0,0 +1,33 @@
+#* 1137. N-th Tribonacci Number
+n = 6
+# @param {Integer} n
+# @return {Integer}
+#*
+def tribonacci(n)
+  a,b,c = 0,1,1
+  return [a,b,c][n] if n < 3
+  (n-2).times {a,b,c = b,c,a+b+c} and c
+end
+p tribonacci(n)
+
+#* READABLE
+def tribonacci(n)
+  a,b,c = 0,1,1
+  return [a,b,c][n] if n < 3
+  (n-2).times do 
+    a,b,c = b,c,a+b+c
+  end
+  c
+end
+p tribonacci(n)
+
+#*
+def tribonacci(n)
+    t = [0, 1, 1]
+    until n < 3
+        t.rotate![2] = t.sum
+        n -= 1
+    end
+    t[n]
+end
+p tribonacci(n)
